2e6235f0cae9b5934356e29594b0e6cea7d15bb3
HEROgold <martijnwieringa28@gmail.com>
2026-02-01 18:01:41 +0100

    Add reference extension

---
+++ examples/references.py
@@ -0,0 +1,33 @@
+"""
+This examples shows how to use references to link configuration files.
+"""
+from confkit.ext.reference import Reference
+from pathlib import Path
+from configparser import ConfigParser
+from confkit import Config
+
+class OtherConfig(Config):
+    pass
+
+# Set up the parser and file
+parser = ConfigParser()
+Config.set_parser(parser)
+OtherConfig.set_parser(ConfigParser())
+Config.set_file(Path(__file__).parent / "ref.ini")
+OtherConfig.set_file(Path(__file__).parent / "other_ref.ini")
+
+class AppConfig:
+    ref = OtherConfig(Reference(Config))
+    other_ref = Config(Reference(OtherConfig))
+    some_value_1 = Config("value1")
+    some_value_2 = OtherConfig("value2")
+    some_value_3 = Config("value3")
+
+if __name__ == "__main__":
+    cfg = AppConfig()
+    print(cfg.ref)
+    cfg.ref = Config
+    print(cfg.ref)
+    print(cfg.other_ref)
+    cfg.other_ref = OtherConfig
+    print(cfg.other_ref)
---
+++ src/confkit/ext/reference.py
@@ -0,0 +1,42 @@
+"""Extension to confkit adds reference-related functionality.
+
+It allows you to create relative references to other configuration files
+"""
+
+from pathlib import Path
+from typing import Any
+
+from confkit import Config
+from confkit.data_types import BaseDataType
+
+ConfigType = type[Config[Any]]
+
+
+class Reference(BaseDataType[ConfigType]):
+    """Data type for referencing other configuration.
+
+    This data type allows you to create relative references to other
+    configuration files within the confkit framework.
+    """
+
+    def __str__(self) -> str:
+        """Return the string representation of the reference."""
+        return f"{self.value._file}"  # noqa: SLF001
+
+    def convert(self, value: str) -> ConfigType:
+        """Convert a string value to the desired type."""
+        file_path = Path(value)
+        cls = self.value
+        cls.set_file(file_path.resolve())
+        return cls
+
+    def __set__(self, instance: object, value: ConfigType) -> None:
+        """Set the value on the owner configuration instance."""
+        if not isinstance(value, type) or not issubclass(value, Config):
+            msg = "Reference data type can only be set to Config<class>."
+            raise TypeError(msg)
+        self.value = value
+
+    def __get__(self, instance: object, owner: type) -> ConfigType:
+        """Get the value from the owner configuration instance."""
+        return self.value
